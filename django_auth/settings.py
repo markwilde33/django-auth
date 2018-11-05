c5c704e4f197a00bc6225d3409263184ee9c539a
Mark Wilde <markwilde29@gmail.com>
2018-11-05 19:45:50 +0000

    Upto date with 'Sending A Real Email' module

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -120,6 +120,18 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+
+# EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/1.11/howto/static-files/
+
+STATIC_URL = '/static/'
+
 MESSAGE_STORAGE = "django.contrib.messages.storage.session.SessionStorage"
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = os.environ.get("EMAIL_ADDRESS")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
+EMAIL_PORT = 587
